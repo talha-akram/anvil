684153921b585bef794adde0aa39f84e4be6200d
Talha Akram <talha2891@gmail.com>
2021-11-29 19:10:38 +0500

    moved packer.nvim bootstrap logic to top to avoid errors in resolving dependencies while loading init.lua

--- init.lua
+++ init.lua
@@ -3,6 +3,19 @@
 local fn = vim.fn
 local vim_cmd = vim.cmd
 
+-- Only load plugins when not runing as root
+if (fn.exists('$SUDO_USER') == 0) then
+  local install_path = fn.stdpath('data') .. '/site/pack/packer/start/packer.nvim'
+  local run_sync = false
+
+  -- install packer for package management, if missing
+  if (fn.empty(fn.glob(install_path)) > 0) then
+    run_sync = fn.system({'git', 'clone', '--depth', '1', 'https://github.com/wbthomason/packer.nvim', install_path})
+  end
+
+  require('plugins').setup(run_sync)
+end
+
 vim.cmd([[
   " Set fish as default shell
   set shell=fish
@@ -22,17 +35,4 @@ vim.cmd([[
 
 -- Configure NeoVim
 require('configuration').setup()
-
--- Only load plugins when not runing as root
-if (fn.exists('$SUDO_USER') == 0) then
-  local install_path = fn.stdpath('data') .. '/site/pack/packer/start/packer.nvim'
-  local run_sync = false
-
-  -- install packer for package management, if missing
-  if (fn.empty(fn.glob(install_path)) > 0) then
-    run_sync = fn.system({'git', 'clone', '--depth', '1', 'https://github.com/wbthomason/packer.nvim', install_path})
-  end
-
-  require('plugins').setup(run_sync)
-end
 
