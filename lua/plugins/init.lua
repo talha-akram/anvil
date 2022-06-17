a7f0debc7a8714c88c74e7e8910f79c5f1dc974d
Talha Akram <talha2891@gmail.com>
2022-06-17 12:53:58 +0500

    fixed typo in user  command definition

--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -56,7 +56,7 @@ local on_startup = function(use)
 
       -- Create User command for opening gitui in neovim, if installed
       if (vim.fn.executable('gitui') == 1) then
-          vim.api.create_user_command('GitUI', 'edit term://gitui', {})
+          vim.api.nvim_create_user_command('GitUI', 'edit term://gitui', {})
       end
     end,
     -- requires = {'tpope/vim-fugitive', 'rbong/vim-flog'}
