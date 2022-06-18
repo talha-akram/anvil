6f7de75650965c15cadecaaf0e2c77ff509c85cb
Talha Akram <talha2891@gmail.com>
2022-06-18 06:57:56 +0500

    added keymaps to telescope lsp actions

--- lua/plugins/lspconfig.lua
+++ lua/plugins/lspconfig.lua
@@ -1,20 +1,30 @@
 local lsp = require('lspconfig');
-local set_keymap = vim.keymap.set
 local capabilities = require('cmp_nvim_lsp').update_capabilities(vim.lsp.protocol.make_client_capabilities())
 
 local on_attach = function()
+  local telescope_builtin = require('telescope.builtin');
+  local set_keymap = function(lhs, rhs)
+    vim.keymap.set('n', lhs, rhs, { noremap = true })
+  end
+
   -- Add keybindings for LSP integration
-  set_keymap('n', '<Leader>j',     '<cmd>lua vim.lsp.buf.declaration()<CR>',      { noremap = true })
-  set_keymap('n', '<Leader><S-d>', '<cmd>lua vim.lsp.buf.definition()<CR>',       { noremap = true })
-  set_keymap('n', '<Leader>h',     '<cmd>lua vim.lsp.buf.hover()<CR>',            { noremap = true })
-  set_keymap('n', '<Leader>i',     '<cmd>lua vim.lsp.buf.implementation()<CR>',   { noremap = true })
-  set_keymap('n', '<Leader>k',     '<cmd>lua vim.lsp.buf.signature_help()<CR>',   { noremap = true })
-  set_keymap('n', '<Leader>d',     '<cmd>lua vim.lsp.buf.type_definition()<CR>',  { noremap = true })
-  set_keymap('n', '<Leader>r',     '<cmd>lua vim.lsp.buf.references()<CR>',       { noremap = true })
-  set_keymap('n', '<Leader>s',     '<cmd>lua vim.lsp.buf.document_symbol()<CR>',  { noremap = true })
-  set_keymap('n', '<Leader>w',     '<cmd>lua vim.lsp.buf.workspace_symbol()<CR>', { noremap = true })
-  set_keymap('n', '<Leader>]',     '<cmd>lua vim.diagnostic.goto_next()<CR>',     { noremap = true })
-  set_keymap('n', '<Leader>[',     '<cmd>lua vim.diagnostic.goto_prev()<CR>',     { noremap = true })
+  set_keymap('<Leader>j', '<cmd>lua vim.lsp.buf.declaration()<CR>')
+  set_keymap('<Leader>d', '<cmd>lua vim.lsp.buf.definition()<CR>')
+  set_keymap('<Leader>h', '<cmd>lua vim.lsp.buf.hover()<CR>')
+  set_keymap('<Leader>i', '<cmd>lua vim.lsp.buf.implementation()<CR>')
+  set_keymap('<Leader>k', '<cmd>lua vim.lsp.buf.signature_help()<CR>')
+  set_keymap('<Leader>td', '<cmd>lua vim.lsp.buf.type_definition()<CR>')
+  set_keymap('<Leader>r', '<cmd>lua vim.lsp.buf.references()<CR>')
+  set_keymap('<Leader>s', '<cmd>lua vim.lsp.buf.document_symbol()<CR>')
+  set_keymap('<Leader>w', '<cmd>lua vim.lsp.buf.workspace_symbol()<CR>')
+  set_keymap('<Leader>]', '<cmd>lua vim.diagnostic.goto_next()<CR>')
+  set_keymap('<Leader>[', '<cmd>lua vim.diagnostic.goto_prev()<CR>')
+  set_keymap('<Leader>lr', telescope_builtin.lsp_references)
+  set_keymap('<Leader>ls', telescope_builtin.lsp_document_symbols)
+  set_keymap('<Leader>lw', telescope_builtin.lsp_workspace_symbols)
+  set_keymap('<Leader>li', telescope_builtin.lsp_implementations)
+  set_keymap('<Leader>ld', telescope_builtin.lsp_definitions)
+  set_keymap('<Leader>ltd', telescope_builtin.lsp_type_definitions)
 end
 
 -- Configure LS for JavaScript
