a5617aeef2ff5a86c06e7a0bcf6949f04181f958
Talha Akram <talha2891@gmail.com>
2022-06-23 14:31:24 +0500

    fixed formatting

--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -16,7 +16,7 @@ if (fn.empty(fn.glob(install_path)) > 0) then
     'https://github.com/wbthomason/packer.nvim',
     install_path
   })
-  vim.cmd [[packadd packer.nvim]]
+  vim.cmd('packadd packer.nvim')
 end
 
 -- Use a protected call to avoid errors on first use
