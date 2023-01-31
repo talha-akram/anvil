8880e7ef233e0c7da1a7253ed86a20af9eeacb45
Talha Akram <talha.akram@ubercarshare.com>
2023-01-31 23:02:02 +0500

    remove everforest in favor of noctis

--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -18,7 +18,6 @@ vim.opt.rtp:prepend(install_path);
 
 require('lazy').setup({
   -- Color schemes
-  'sainnhe/everforest',
   'talha-akram/noctis.nvim',
 
   -- Visualise and control undo history in tree form.
