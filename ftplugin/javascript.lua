d56d73e586a1ec2fb10ecc08f540776be26f34ec
Talha Akram <talha2891@gmail.com>
2024-12-22 00:19:46 +0500

    Add support for quick lint js

--- ftplugin/javascript.lua
+++ ftplugin/javascript.lua
@@ -1,18 +1,11 @@
 local options = require('configuration.lsp');
 
+require('ftplugin.typescript')
+
 vim.lsp.start({
-  name = 'ts-ls',
-  cmd = {'typescript-language-server', '--stdio'},
-  filetypes = {
-    'javascript',
-    'javascriptreact',
-    'javascript.jsx',
-    'typescript',
-    'typescriptreact',
-    'typescript.tsx',
-  },
+  cmd = { 'quick-lint-js', '--lsp-server' },
+  filetypes = { 'javascript', 'typescript' },
   root_dir = vim.fs.root(0, {'tsconfig.json', 'jsconfig.json', 'package.json', '.git'}),
-  init_options = {hostInfo = 'neovim'},
   single_file_support = true,
   on_attach = options.on_attach,
   capabilities = options.capabilities,
