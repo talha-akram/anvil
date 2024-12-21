0b6aa3333ff46379a4773fc7f9f37b47b8e321c5
Talha Akram <talha2891@gmail.com>
2024-12-22 03:05:38 +0500

    Enable deleting a range of qf items

--- ftplugin/qf.lua
+++ ftplugin/qf.lua
@@ -20,3 +20,25 @@ vim.keymap.set('n', 'dd', function()
   local winid = vim.fn.win_getid() -- Get the window ID of the quickfix window
   vim.api.nvim_win_set_cursor(winid, {new_idx, 0})
 end, {buffer = true})
+
+vim.keymap.set('x', 'd', function()
+  local qf_list = vim.fn.getqflist()
+
+  -- Get selected range in visual mode
+  local starts = vim.fn.line('v')
+  local ends = vim.fn.line('.')
+  local from = math.min(starts, ends)
+  local to = math.max(starts, ends)
+
+  -- Remove entries from the quickfix list
+  for index = from, to, 1 do
+    table.remove(qf_list, index)
+    log_to_file(string.format("removed: %s", index))
+  end
+
+  -- Replace the quickfix list with the updated list
+  vim.fn.setqflist(qf_list, "r")
+
+  -- Refresh the quickfix window
+  vim.cmd("copen")
+end, {buffer = true})
