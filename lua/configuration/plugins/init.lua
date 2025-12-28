daad405c69d666456662edd8eb40caeb1432e56b
Talha Akram <talha2891@gmail.com>
2025-12-29 03:19:19 +0500

    fix typos.

--- lua/configuration/plugins/init.lua
+++ lua/configuration/plugins/init.lua
@@ -16,16 +16,16 @@ vim.pack.add({
 },{
   load = function(plug)
     local data = plug.spec.data or {}
-    local setup = data.setup
 
     vim.cmd.packadd(plug.spec.name)
 
+    local setup = data.setup
     if setup ~= nil and type(setup) == 'function' then
       setup()
     end
 
     local build_cmd = data.build_cmd
-    if build ~= nil and type(setup) == 'string' then
+    if build_cmd  ~= nil and type(build_cmd) == 'string' then
       vim.cmd(build_cmd)
     end
   end
